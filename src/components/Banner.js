dd44c863f919889d1546612f18504fdb2f1dbc63
Kourtney Queeley <Kourtney_queeley@hotmail.com>
2023-06-08 22:40:54 +0100

    Styled Banner

---
+++ src/components/Banner.js
@@ -0,0 +1,9 @@
+export const Banner = () => {
+    return(
+        <div className="jumbotron">
+            <h1>Take Home Salary Calculator</h1>
+            <hr/>
+            <h2 className="fs-6">Calculate your take home salary</h2>
+        </div>
+    )
+}
---
+++ src/components/CalculatorForm.js
@@ -0,0 +1,7 @@
+export const CalculatorForm = () => {
+    return(
+        <div>
+            CalculatorForm
+        </div>
+    )
+}
